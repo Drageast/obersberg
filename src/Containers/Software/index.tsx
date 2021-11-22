c472bd24105286ed437f941fc948339fbdda8f4c
LauriNator188 <63661901+LauriNator188@users.noreply.github.com>
2021-11-22 08:34:46 +0100

    Update index.tsx

--- src/Containers/Software/index.tsx
+++ src/Containers/Software/index.tsx
@@ -21,10 +21,13 @@ export function Desktop() {
                         <Foreground CustomCSS={ css.CustomCSSContent } Opacity={ 50 }>
                             <h3>Wir bieten:</h3>
 
-                            <p> Wir schützen sie vor dubiosen Angeboten</p>
-                            <p> Wir schützen sie vor dubiosen Webseiten</p>
-                            <p> Wir filtern diverse Downloads</p>
-                            <p> Wir überwachen die Netzwerkverbindung auf unauthorisierte Zugriffe</p>
+                            <p> Antimalware Software, die Schadsoftware und Bedrohungen wie
+                                Viren, Trojaner, Adware, Bots, Rogue-Dateien, Spyware oder Ransomware erkennt.
+                                Wir durchsuchen den Computer nach solchen Schädlingen, entfernt sie oder stellt
+                                sie unter Quarantäne. Sollte der Rechner außerdem durch Rootkits befallen und
+                                beschädigt worden sein, kann unser System diese ebenfalls entfernen und eventuelle
+                                Schäden als Folgen des Angriffes beheben.</p>
+                            
 
                             <Button4 CustomCSS={ css.ButtonStyles } Element={<h3>Kaufen</h3>}/>
                         </Foreground>
@@ -40,10 +43,13 @@ export function Desktop() {
                         <Foreground CustomCSS={ css.CustomCSSContent } Opacity={ 60 }>
                             <h3>Wir bieten:</h3>
 
-                            <p> unser Haus-eigenes System</p>
-                            <p> Systeme für private PC's und auch Datenbanken</p>
-                            <p> Schnelle funktionsweise</p>
-                            <p> Sehr starke Verschlüsselung</p>
+                            <p> Wir benutzen das One-Time-Pad Verfahren. Dateien sind dadurch nur sehr schwer oder überhaupt
+                                nicht dechiffrierbar. Die Schlüssel zum Code sind dabei völlig zufällig erstellt und werden
+                                jeweils nur für eine Datei verwendet. Nach einmaliger Verwendung des Schlüssels wird der Schlüssel
+                                sofort gelöscht und es wird ein anderer Schlüssel verwendet. So garantieren wir absolute Sicherheit
+                                für ihre Daten.
+                            </p>
+                            
 
                             <Button4 CustomCSS={ css.ButtonStyles } Element={<h3>Kaufen</h3>}/>
                         </Foreground>
